955f717edee05e461bc6698e9dcf6e7a568fbb72
Bartosz <bartoszbachul1@gmail.com>
2025-01-25 16:58:28 +0100

    fixed not working things

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,12 +3,11 @@
 
 #include <iomanip>
 #include <iostream>
-#include <thread>
 
 using json = nlohmann::json;
 
- 
 int main()
 {
-    std::cout << std::setw(4) << json::meta() << std::endl;
+    std::cout << std::setw(4) << json::meta() << '\n';
 }
+
