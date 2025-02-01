a640037ca471e3f32fa4401722521189a5eba763
Kosciarz <bartoszbachul1@gmail.com>
2025-02-01 14:45:54 +0100

    completed first task of the challenge: get request path

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,16 +9,49 @@
 
 using namespace boost::asio::ip;
 
+static std::string GetRequestedPath(std::span<char> request_buffer)
+{
+    std::string requested_path{};
+    for (auto it = std::ranges::find(request_buffer, '/'); *it != ' '; ++it)
+        requested_path += *it;
+    return requested_path;
+}
+
 static void HandleRequest(tcp::socket socket)
 {
     try
     {
-        std::array<char, 1024 * 5> buffer{};
+        std::array<char, 1024 * 5> request_buffer{};
 
-        auto bytes_read = socket.read_some(boost::asio::buffer(buffer.data(), buffer.size()));
+        socket.wait(tcp::socket::wait_read);
 
-        std::cout << "Request: " << std::string(buffer.data(), bytes_read) << '\n';
+        socket.async_read_some(boost::asio::buffer(request_buffer.data(), request_buffer.size()),
+            [&](const boost::system::error_code& ec, std::size_t bytes_read)
+            {
+                if (!ec)
+                    std::cout << "Bytes read: " << bytes_read << '\n';
+                else
+                    std::cerr << "Error: " << ec.message() << '\n';
+            }
+        );
 
+        std::cout << "Request: " << std::string(request_buffer.data(), request_buffer.size()) << '\n';
+
+        std::string requested_path = GetRequestedPath(request_buffer);
+        
+        const std::string reply = "HTTP/1.1 200 OK\r\n\r\nRequested path: " + requested_path + "\r\n";
+
+        socket.async_write_some(boost::asio::buffer(reply.data(), reply.size()),
+            [&](const boost::system::error_code& ec, std::size_t bytes_written)
+            {
+                if (!ec)
+                    std::cout << "Bytes written: " << bytes_written << '\n';
+                else
+                    std::cerr << "Error: " << ec.message() << '\n';
+            }    
+        );
+
+        socket.close();
     }
     catch (const std::exception& e)
     {
@@ -28,15 +61,22 @@ static void HandleRequest(tcp::socket socket)
 
 int main()
 {
-    boost::asio::io_context io_context;
-
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 80));
-    std::cout << "Listening on port 80... " << '\n';
-
-    while (true)
+    try
     {
-        tcp::socket socket(io_context);
-        acceptor.accept(socket);
-        HandleRequest(std::move(socket));
+        boost::asio::io_context io_context;
+
+        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 8080));
+        std::cout << "Listening on port 80... " << '\n';
+
+        while (true)
+        {
+            tcp::socket socket(io_context);
+            acceptor.accept(socket);
+            HandleRequest(std::move(socket));
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << '\n';
     }
 }
