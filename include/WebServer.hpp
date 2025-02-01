ab8e6b45f2f76f196d62ba029094a1fa7225b165
Bartosz <bartoszbachul1@gmail.com>
2025-02-01 22:36:46 +0100

    added the WebServer class to better handle functionality

--- include/WebServer.hpp
+++ include/WebServer.hpp
@@ -1,10 +1,35 @@
 ﻿#ifndef WEBSERVER_H
 #define WEBSERVER_H
 
+#include <boost/asio.hpp>
+
 #include <iostream>
+#include <array>
+
+using namespace boost::asio::ip;
 
 class WebServer
 {
+public:
+    WebServer(boost::asio::io_context& io_context)
+        : m_acceptor(io_context, tcp::endpoint(tcp::v4(), 8080)),
+        m_socket(io_context)
+    {
+        std::cout << "Listening on port 8080... " << '\n';
+        StartAccept();
+    }
+
+private:
+    void StartAccept();
+
+    void ReadRequest();
+
+    void HandleRequest();
+
+private:
+    std::array<char, 1024 * 5> m_request_buffer{};
+    tcp::acceptor m_acceptor;
+    tcp::socket m_socket;
 };
 
 #endif // WEBSERVER_H
--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -1,2 +1,81 @@
 #include "WebServer.hpp"
 
+#include <algorithm>
+#include <array>
+#include <string>
+
+void WebServer::StartAccept()
+{
+    m_request_buffer.fill(0);
+    m_acceptor.async_accept(m_socket,
+        [this](const boost::system::error_code& ec)
+        {
+            if (!ec)
+            {
+                std::cout << "Client connected!" << '\n';
+                HandleRequest();
+            }
+            else
+            {
+                std::cerr << "Error: " << ec.message() << '\n';
+                m_socket.close();
+            }
+            StartAccept();
+        });
+}
+
+void WebServer::ReadRequest()
+{
+    boost::asio::async_read(m_socket,
+        boost::asio::buffer(m_request_buffer.data(), m_request_buffer.size()),
+        [this](const boost::system::error_code& ec, std::size_t bytes_read)
+        {
+            if (!ec)
+            {
+                std::cout << "Bytes read: " << bytes_read << '\n';
+                std::cout << "Request: " << std::string(m_request_buffer.data(), bytes_read) << '\n';
+
+                HandleRequest();
+            }
+            else
+            {
+                std::cerr << "Error: " << ec.message() << '\n';
+                m_socket.close();
+            }
+        });
+}
+
+void WebServer::HandleRequest()
+{
+    std::string requested_path{};
+    if (auto it = std::ranges::find(m_request_buffer, '/'); it != m_request_buffer.end())
+    {
+        for (; *it != ' '; ++it)
+        {
+            requested_path += *it;
+        }
+    }
+
+    const std::string reply =
+        "HTTP/1.1 200 OK\r\n" +
+        std::format("Content-Length: {}\r\n", requested_path.size()) +
+        "Content-Type: text/plain\r\n"
+        "\r\n" +
+        std::format("Requested path: {}\r\n", requested_path);
+
+    boost::asio::async_write(m_socket,
+        boost::asio::buffer(reply.data(), reply.size()),
+        [this](const boost::system::error_code& ec, std::size_t bytes_written)
+        {
+            if (!ec)
+            {
+                std::cout << "Response sent!" << '\n';
+                m_socket.close();
+            }
+            else
+            {
+                std::cerr << "Error: " << ec.message() << '\n';
+                m_socket.close();
+            }
+        });
+}
