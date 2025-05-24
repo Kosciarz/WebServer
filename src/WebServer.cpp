6945330b5ab04f5531dc483af5793b9e76301f99
Bartosz <bartoszbachul1@gmail.com>
2025-05-24 14:34:38 +0200

    Add the static files path in WebServer class

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <filesystem>
 
 #include <asio/ip/tcp.hpp>
 #include <asio/error_code.hpp>
@@ -8,10 +9,12 @@
 #include "WebServer.hpp"
 
 using namespace asio::ip;
+namespace fs = std::filesystem;
 
 WebServer::WebServer(asio::io_context& ioContext)
     : m_IoContext{ioContext},
-      m_Acceptor{ioContext, tcp::endpoint(tcp::v4(), 8080)}
+      m_Acceptor{ioContext, tcp::endpoint(tcp::v4(), 8080)},
+      m_StaticFilesRoot{FindServerRoot() / "www"}
 {
     std::cout << "Listening on port 8080..." << '\n';
     StartAccept();
@@ -19,7 +22,7 @@ WebServer::WebServer(asio::io_context& ioContext)
 
 void WebServer::StartAccept()
 {
-    auto connection = TcpConnection::Create(m_IoContext);
+    auto connection = TcpConnection::Create(m_IoContext, m_StaticFilesRoot);
     m_Acceptor.async_accept(
         connection->Socket(),
         [this, connection](const asio::error_code& ec)
@@ -37,4 +40,18 @@ void WebServer::HandleAccept(const TcpConnection::Pointer& connection, const asi
         std::cerr << "Accept error: " << ec.message() << '\n';
 
     StartAccept();
+}
+
+fs::path WebServer::FindServerRoot()
+{
+    auto path = fs::current_path();
+    while (path.has_relative_path() && path.filename() != "WebServer")
+    {
+        if (path == path.root_path())
+        {
+            return {};
+        }
+        path = path.parent_path();
+    }
+    return path;
 }
--- src/WebServer.hpp
+++ src/WebServer.hpp
@@ -1,5 +1,7 @@
 ﻿#pragma once
 
+#include <filesystem>
+
 #include <asio/ip/tcp.hpp>
 #include <asio/error_code.hpp>
 
@@ -15,7 +17,11 @@ private:
 
     void HandleAccept(const TcpConnection::Pointer& connection, const asio::error_code& ec);
 
+    static std::filesystem::path FindServerRoot();
+
 private:
     asio::io_context& m_IoContext;
     asio::ip::tcp::acceptor m_Acceptor;
+
+    std::filesystem::path m_StaticFilesRoot;
 };
